247be808759cceb87a20ecb747e48fcb601ad71d
MarkoNS1990 <marczecevic@gmail.com>
2020-09-21 11:11:50 +0200

    Added AddBtn and AddLogModal

---
+++ src/components/layout/AddBtn.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AddBtn = () => {
+  return (
+    <div className="fixed-action-btn">
+      <a
+        href="#add-log-modal"
+        className="btn-floating btn-large blue darken-2 modal-trigger"
+      >
+        <i className="large material-icons">add</i>
+      </a>
+      <ul>
+        <li>
+          <a
+            href="#tech-list-modal"
+            className="btn-floating green modal-trigger"
+          >
+            <e className="material-icons">person</e>
+          </a>
+        </li>
+        <li>
+          <a href="#tech-modal" className="btn-floating red modal-trigger">
+            <e className="material-icons">person_add</e>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AddBtn;
